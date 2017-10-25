ad0058c3059eea4af8a30f865a67712bafc5b954
myIrelia <myIrelia@foxmail.com>
2017-10-25 14:15:34 +0800

    Initial commit

---
+++ src/test/java/com/didispace/ApplicationTests.java
@@ -0,0 +1,35 @@
+package com.didispace;
+
+import com.didispace.service.UserService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+public class ApplicationTests {
+
+	@Autowired
+	private UserService userSerivce;
+
+
+	@Test
+	public void test() throws Exception {
+		// 插入5个用户
+		userSerivce.create("a", 1);
+		userSerivce.create("b", 2);
+		userSerivce.create("c", 3);
+		userSerivce.create("d", 4);
+		userSerivce.create("e", 5);
+
+
+
+	}
+
+
+}
